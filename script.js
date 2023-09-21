3e38b19cae1cd67f20f810bc4ecb6adc3615f450
jacodegithub <dudeobama246@gmail.com>
2023-09-21 17:00:43 +0530

    created a photo expanding project using js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+const panels = document.querySelectorAll('.panel')
+console.log(panels)
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        if(panel.classList.contains('active')) {
+            panel.classList.remove('active');
+        }
+        else {
+            removeActiveClasses();
+            panel.classList.add('active');
+        }
+    })
+})
 
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
 
