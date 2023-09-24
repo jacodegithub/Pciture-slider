c09ce280aa558381a43043e82b2cdf4665e977bc
jacodegithub <dudeobama246@gmail.com>
2023-09-25 01:47:31 +0530

    added functionalities

--- 04-js-rotating nav/script.js
+++ 04-js-rotating nav/script.js
@@ -1,3 +1,11 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const 
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav')
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+})
