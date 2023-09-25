4dac67c6342c56f183e8e17a23a18654be658484
jacodegithub <dudeobama246@gmail.com>
2023-09-26 01:01:30 +0530

    added js functionalities

---
+++ 05-js-blur loading/script.js
@@ -0,0 +1,25 @@
+const img = document.querySelector('.img')
+const loadText = document.querySelector('.loading-text')
+
+let load = 0;
+
+let int = setInterval(blurring, 10)
+
+function blurring() {
+    load++;
+
+    if(load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerHTML = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    img.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    console.log(load)
+}
+
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
